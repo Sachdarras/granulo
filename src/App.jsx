98fefbbad21408b01d8cdd3443d9f7a398a16028
Sacha Darras <sachdarras@gmail.com>
2024-09-26 13:27:04 +0200

    responsive +logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./app.scss";
 import Nav from "./components/Nav";
+import logo from "./assets/logo/logo.png"; 
 
 function App() {
   return (
     <div>
       <header>
         <h1 className="mainlogo">
-          <img src="" alt="logo granulo" />
+          <img src={logo} alt="logo granulo" /> 
         </h1>
         <Nav />
       </header>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,26 @@
+import React, { useState } from 'react';
+
+
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navi">
-      <a href="/home">Accueil</a>
-      <a href="/equipe">Equipe</a>
-      <a href="/contact">Contact</a>
+      {!isOpen && ( // Affiche l'icône seulement si le menu n'est pas ouvert
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776; {/* Symbole du menu burger */}
+        </div>
+      )}
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <button className="close-button" onClick={toggleMenu}>&times;</button> {/* Bouton de fermeture */}
+        <a href="/home">Accueil</a>
+        <a href="/equipe">Equipe</a>
+        <a href="/contact">Contact</a>
+      </div>
     </nav>
   );
 }
