1a25c366a6879351fcbd2f87a6752e6dc4207e3f
Sacha Darras <sachdarras@gmail.com>
2024-09-26 14:21:36 +0200

    debug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Importer Link
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,9 +20,9 @@ function Nav() {
           &times;
         </button>{" "}
         {/* Bouton de fermeture */}
-        <a href="/home">Accueil</a>
-        <a href="/equipe">Equipe</a>
-        <a href="/contact">Contact</a>
+        <Link to="/home" onClick={toggleMenu}>Accueil</Link>
+        <Link to="/equipe" onClick={toggleMenu}>Équipe</Link>
+        <Link to="/contact" onClick={toggleMenu}>Contact</Link>
       </div>
     </nav>
   );
